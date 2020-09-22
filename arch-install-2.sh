a9a1261eb7458fbe0f697991178734ee2149f2b1
ahvth <andrewaslan@outlook.com>
2020-09-22 13:50:58 +0200

    cleanup

--- arch-install-2.sh
+++ arch-install-2.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#timedatectl set-ntp true
-#mkfs.fat -F 32 /dev/sda1
-#mkfs.ext4 /dev/sda2
-#mount /dev/sda2 /mnt
-#reflector --country Hungary --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-#pacstrap /mnt base base-devel linux linux-firmware
-#genfstab -U /mnt >> /mnt/etc/fstab
-#arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/Europe/Budapest /etc/localtime
 hwclock --systohc
 sed 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen > /etc/locale.gen
@@ -19,14 +11,13 @@ passwd dev
 mkdir /home/dev
 chown -R dev /home/dev
 sed 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers > /etc/sudoers
-yes | pacman -S dialog xorg xorg-xinit xorg-xauth xterm
+pacman --noconfirm -S dialog xorg xorg-xinit xorg-xauth xterm
 yes | pacman -S efibootmgr
 mkdir /efi
 mount /dev/sda1 /efi
 grub-install --target=x86_64-efi --efi-directory=efi --bootloader-id=ARCH
 grub-mkconfig -o /boot/grub/grub.cfg
-yes | pacman --noconfirm -S gnome
+pacman --noconfirm -S gnome
 systemctl enable gdm
 systemctl enable NetworkManager.service
 exit
-echo "Installation successful. Reboot now to use your system."
--- arch-install.sh
+++ arch-install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#timedatectl set-ntp true
-#mkfs.fat -F 32 /dev/sda1
-#mkfs.ext4 /dev/sda2
-#mount /dev/sda2 /mnt
-#reflector --country Hungary --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-#pacstrap /mnt base base-devel linux linux-firmware
-#genfstab -U /mnt >> /mnt/etc/fstab
+timedatectl set-ntp true
+mkfs.fat -F 32 /dev/sda1
+mkfs.ext4 /dev/sda2
+mount /dev/sda2 /mnt
+reflector --country Hungary --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+pacstrap /mnt base base-devel linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
 cp arch-install-2.sh /mnt/arch-install-2.sh
 arch-chroot /mnt /arch-install-2.sh
