e591543d1354d492c83aec27ddacfca1c39d0880
ahvth <andrewaslan@outlook.com>
2020-09-22 23:35:19 +0200

    merge parser into script

--- install.sh
+++ install.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
 
+# load config file keys into environment variables
+while read line; do
+  if [[ "$line" =~ ^[^#]*= ]]; then
+    key=`echo $line | sed 's/=.*//g'`
+    value=`echo $line | sed 's/^.*.=//g'`
+    export "$key"="$value"
+    ((i++))
+  fi
+done < archconfig
+
 timedatectl set-ntp true
-mkfs.fat -F 32 /dev/sda1
-mkfs.ext4 /dev/sda2
+
+# case for partition layout
+# single partition config
+# TODO: parted commands here
+mkfs.fat -F 32 /dev/"$DEVICE"1
+mkfs.ext4 /dev/"$DEVICE"2
 mount /dev/sda2 /mnt
-reflector --country Hungary --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+# home partition config (home folder in a separate partition)
+
+# btrfs partition config (uses the btrfs filesystem with subvolumes in ubuntu layout @ and @home)
+
+
+# sort mirrors by speed in country
+if [ $SORTMIRRORS == "true" ]; then
+ reflector --country "$MIRRORCOUNTRY" --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+fi
+
+# case for lts vs mainline kernel
 pacstrap /mnt base base-devel linux linux-firmware
+
 genfstab -U /mnt >> /mnt/etc/fstab
 cp chroot.sh /mnt/chroot.sh
+cp archconfig /mnt/archconfig
 arch-chroot /mnt /chroot.sh
